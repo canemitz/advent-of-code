c0c1c5dd2dd41237795ddfb3dd8e20c5ab225e60
Clayton Nemitz <clay.nemitz@gmail.com>
2021-12-08 14:04:47 -0500

    Add day 5, part 2 solution.

--- 2021/day5.py
+++ 2021/day5.py
@@ -5,10 +5,9 @@ def part1(input_data):
     print('Q: Consider only horizontal and vertical lines. At how many points do at least two lines overlap?')
 
     line_segments = get_line_segments(input_data)
-    line_segments_straight = get_straight_line_segments(line_segments)
 
-    diagram = get_initial_diagram(line_segments_straight)
-    diagram_marked = mark_straight_lines_on_diagram(diagram, line_segments_straight)
+    diagram = get_initial_diagram(line_segments)
+    diagram_marked = mark_straight_lines_on_diagram(diagram, line_segments)
 
     ans = get_num_points_with_x_lines(diagram_marked, x_lines=2)
 
@@ -17,9 +16,8 @@ def part1(input_data):
 
 def get_line_segments(input_data):
     """Return list of dictionaries of coordinates for each segment.
-    Reorder direction so segments go from lesser to greater values.
     Example:
-        input_data = '5,9 -> 0,9'
+        input_data = '0,9 -> 5,9'
         line_segments = [{'x1': 0, 'y1': 9, 'x2': 5, 'y2': 9}]
     """
     line_segments_list = ''.join(input_data.readlines()).split('\n')
@@ -27,13 +25,13 @@ def get_line_segments(input_data):
 
     line_segments = []
     for segment in line_segments_list:
-        (x_a, y_a) = segment[0].split(',')
-        (x_b, y_b) = segment[1].split(',')
+        (x_1, y_1) = segment[0].split(',')
+        (x_2, y_2) = segment[1].split(',')
         segment_dict = {
-            'x1': min(int(x_a), int(x_b)),
-            'x2': max(int(x_a), int(x_b)),
-            'y1': min(int(y_a), int(y_b)),
-            'y2': max(int(y_a), int(y_b))
+            'x1': int(x_1),
+            'x2': int(x_2),
+            'y1': int(y_1),
+            'y2': int(y_2)
         }
         line_segments.append(segment_dict)
 
@@ -53,27 +51,38 @@ def get_initial_diagram(line_segments):
     return initial_diagram
 
 
-def get_straight_line_segments(line_segments):
-    """Return only line segments where x1=x2 or y1=y2"""
-    line_segments_straight = []
+def get_filtered_line_segments(line_segments, line_filter):
+    """Filter out line segments where x1=x2 or y1=y2
+    Return those if line_filter is straight, else return the other segments
+    """
+    line_segments_filtered = []
 
     for segment in line_segments:
-        if segment['x1'] == segment['x2'] or segment['y1'] == segment['y2']:
-            line_segments_straight.append(segment)
+        if (segment['x1'] == segment['x2'] or segment['y1'] == segment['y2']):
+            if line_filter == 'straight':
+                line_segments_filtered.append(segment)
+        elif line_filter == 'diagonal':
+            line_segments_filtered.append(segment)
 
-    return line_segments_straight
+    return line_segments_filtered
 
 
 def mark_straight_lines_on_diagram(diagram, line_segments):
-    """Increment elements on diagram where line segments exist."""
+    """Increment elements on diagram where straight line segments exist."""
     diagram_marked = np.array(diagram)
+    line_segments_straight = get_filtered_line_segments(line_segments, 'straight')
 
-    for segment in line_segments:
-        x_range = [x for x in range(segment['x1'], segment['x2']+1)]
-        y_range = [y for y in range(segment['y1'], segment['y2']+1)]
+    for segment in line_segments_straight:
+        x1 = segment['x1']
+        x2 = segment['x2']
+        y1 = segment['y1']
+        y2 = segment['y2']
 
-        for x in x_range:
-            for y in y_range:
+        x_increment = 1 if x1 < x2 else -1
+        y_increment = 1 if y1 < y2 else -1
+
+        for x in range(x1, x2+x_increment, x_increment):
+            for y in range(y1, y2+y_increment, y_increment):
                 diagram_marked[x, y] += 1
 
     return diagram_marked
@@ -91,5 +100,34 @@ def get_num_points_with_x_lines(diagram, x_lines):
 
 
 def part2(input_data):
-    print('Q:')
+    print('Q: Consider all of the lines. At how many points do at least two lines overlap?')
+
+    line_segments = get_line_segments(input_data)
+
+    diagram = get_initial_diagram(line_segments)
+    diagram_marked = mark_straight_lines_on_diagram(diagram, line_segments)
+    diagram_marked = mark_diagonal_lines_on_diagram(diagram_marked, line_segments)
+
+    ans = get_num_points_with_x_lines(diagram_marked, x_lines=2)
     print(f'A: {ans}')
+
+
+def mark_diagonal_lines_on_diagram(diagram, line_segments):
+    """Increment elements on diagram where diagonal line segments exist."""
+    diagram_marked = np.array(diagram)
+
+    line_segments_diagonal = get_filtered_line_segments(line_segments, 'diagonal')
+
+    for segment in line_segments_diagonal:
+        x1 = segment['x1']
+        x2 = segment['x2']
+        y1 = segment['y1']
+        y2 = segment['y2']
+
+        x_increment = 1 if x1 < x2 else -1
+        y_increment = 1 if y1 < y2 else -1
+
+        for x, y in zip(range(x1, x2+x_increment, x_increment), range(y1, y2+y_increment, y_increment)):
+            diagram_marked[x, y] += 1
+
+    return diagram_marked
