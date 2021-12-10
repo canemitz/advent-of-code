c19258ed61f3686351914a625c24575cee843aa1
Clayton Nemitz <clay.nemitz@gmail.com>
2021-12-09 23:03:09 -0500

    Finish day 8, part 2 solution.

--- 2021/day8.py
+++ 2021/day8.py
@@ -57,76 +57,124 @@ def part2(input_data):
     # Loop over the input data
     for dataset in input_data_list:
         signal_patterns = dataset[0].split(' ')
-        output_value = dataset[1].split(' ')
+        output_value_patterns = dataset[1].split(' ')
 
-        dataset_mapping = {}
+        # Determine the segment label sets for each improperly labeled digit
+        digit_segment_sets = get_digit_segment_sets(signal_patterns)
 
-        # Loop over the signal patterns and output value to determine the uniquely defined digits
-        for val in signal_patterns+output_value:
-            digit_for_pattern = get_digit_for_unique_pattern(val)
+        # Use the digit_segment_sets to find the output_value
+        output_value_str = ''
+        for pattern in output_value_patterns:
+            pattern_set = set(pattern)
 
-            # If pattern matches unique digit, set signal wire labels as value of digit key in dataset_mapping
-            if digit_for_pattern:
-                dataset_mapping[digit] = [signal_wire_label for signal_wire_label in val]
+            # Append to output_value_str the digit whose segment set matches this pattern set
+            for digit, digit_segment_set in digit_segment_sets.items():
+                if digit_segment_set == pattern_set:
+                    output_value_str += str(digit)
+                    break
 
-        signal_wire_labels = get_signal_wire_labels(dataset_mapping)
-
-        # use that info to determine the non-unique [-ly defined by number of segments] digits
-        remaining_signal_wire_labels = get_remaining_signal_wire_labels(signal_wire_labels, dataset)
-
-        # ...which means we have all the labels now
-        complete_signal_wire_labels = signal_wire_labels + remaining_signal_wire_labels
-
-        # so, append the output value (using the derived labels) to our list of output_values
-        output_values.append(get_output_value(complete_signal_wire_labels))
+        output_values.append(int(output_value_str))
 
     ans = sum(output_values)
     print(f'A: {ans}')
 
 
-def get_signal_wire_labels(dataset_mapping):
-    # Having gotten all possible uniquely defined digits, determine which segments have which label
-    # somethign something venn diagram
-    # digit 1 has two wires
-    # digit 7 has three wires
-    # digit 4 has four wires
-    # digit 8 has seven wires
-    # given that info, and given dataset_mapping, loop over something (maybe multiple times)
-    #  and figure out which signal wire (given correct positions a through g) has which incorrect/temporary label
-    return signal_wire_labels
+def get_unique_digit_segment_sets(signal_patterns):
+    """Return dictionary of digits (which have unique signal patterns)
+       keyed to the sets of segment labels that correspond to them.
+    """
+    unique_digit_segment_sets = {}
+
+    for signal_pattern in signal_patterns:
+        unique_digit = get_digit_for_unique_pattern(signal_pattern)
+
+        # If digit is uniquely defined, add set of signal wire labels to the dictionary
+        if unique_digit:
+            unique_digit_segment_sets[unique_digit] = {segment_label for segment_label in signal_pattern}
+
+        # Stop if we already have the segment sets for each uniquely defined digit
+        if len(unique_digit_segment_sets) == 4:
+            break
+
+    return unique_digit_segment_sets
 
 
-def get_remaining_signal_wire_labels(signal_wire_labels, dataset):
-    # given signal wire labels gotten from uniquely defined digits...
-    # ...get the other signal wire labels
-    return remaining_signal_wire_labels
+def get_segment_mapping(signal_patterns):
+    # Mapping of "proper" segment label to currently-active segment label
+    segment_mapping = {}
+
+    digit_segment_sets = get_unique_digit_segment_sets(signal_patterns)
+
+    # The segment label appearing in 7, but not in 1, is the proper 'a' segment
+    for segment_label in digit_segment_sets[7]:
+        if segment_label not in digit_segment_sets[1]:
+            segment_mapping['a'] = segment_label
+            break
+    # segment_mapping.keys() == ['a']
+
+    # There are three digits with six segments: 0, 6, and 9
+    # The segment labels not in all three sets are the proper 'c', 'd', and 'e' segments
+    segment_sets_069 = []
+    for signal_pattern in signal_patterns:
+        if len(signal_pattern) == 6:
+            segment_sets_069.append({segment_label for segment_label in signal_pattern})
+    segment_sets_069_union = set.union(segment_sets_069[0], segment_sets_069[1], segment_sets_069[2])
+    segment_sets_069_intersection = set.intersection(segment_sets_069[0], segment_sets_069[1], segment_sets_069[2])
+    segments_cde = segment_sets_069_union - segment_sets_069_intersection
+
+    # The segment label in 7, and in segments_cde, is proper 'c'
+    segment_mapping['c'] = digit_segment_sets[7].intersection(segments_cde).pop()
+    # segment_mapping.keys() == ['a', 'c']
+
+    # The segment label in 4, and in segments_cde, that does not map to proper 'c', is proper 'd'
+    segment_mapping['d'] = digit_segment_sets[4].intersection(segments_cde).difference({segment_mapping['c']}).pop()
+    # segment_mapping.keys() == ['a', 'c', 'd']
+
+    # The segment label in segments_cde, that does not map to proper 'c' or 'd', is proper 'e'
+    segment_mapping['e'] = segments_cde.difference({segment_mapping['c'], segment_mapping['d']}).pop()
+    # segment_mapping.keys() == ['a', 'c', 'd', 'e']
+
+    # The segment label in 1, that does not map to proper 'c', is proper 'f'
+    segment_mapping['f'] = digit_segment_sets[1].difference({segment_mapping['c']}).pop()
+    # segment_mapping.keys() == ['a', 'c', 'd', 'e', 'f']
+
+    # The segment label in 4, that does not map to proper 'c', 'd', or 'f', is proper 'b'
+    segments_cdf = { segment_mapping[x] for x in ['c', 'd', 'f'] }
+    segment_mapping['b'] = digit_segment_sets[4].difference(segments_cdf).pop()
+    # segment_mapping.keys() == ['a', 'b', 'c', 'd', 'e', 'f']
+
+    # The segment in 8 that is not already mapped is proper 'g'
+    segments_abcdef = { x for x in segment_mapping.values() }
+    segment_mapping['g'] = digit_segment_sets[8].difference(segments_abcdef).pop()
+    # segment_mapping.keys() == ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+    return segment_mapping
 
 
-# def get_signal_pattern_digit_mapping(signal_patterns):
-#     dataset_digit_mapping = {}
+def get_digit_segment_sets(signal_patterns):
+    """Return dictionary of digits keyed to the sets of segment labels that correspond to them."""
+    digit_segment_sets = {}
 
-#     while len(dataset_digit_mapping) < 7:
-#         for signal_pattern in signal_patterns:
-#             digit = get_digit_for_unique_pattern(output_value)
+    segment_mapping = get_segment_mapping(signal_patterns)
 
-#     return dataset_digit_mapping
+    # Sets of segments that should correspond to each digit
+    proper_digit_segment_sets = {
+        0: {'a', 'b', 'c',      'e', 'f', 'g'},
+        1: {          'c',           'f'     },
+        2: {'a',      'c', 'd', 'e',      'g'},
+        3: {'a',      'c', 'd',      'f', 'g'},
+        4: {     'b', 'c', 'd',      'f'     },
+        5: {'a', 'b',      'd',      'f', 'g'},
+        6: {'a', 'b',      'd', 'e', 'f', 'g'},
+        7: {'a',      'c',           'f'     },
+        8: {'a', 'b', 'c', 'd', 'e', 'f', 'g'},
+        9: {'a', 'b', 'c', 'd',      'f', 'g'}
+    }
 
+    # Add the mapped segment labels for each digit to the segment set for it
+    for digit in proper_digit_segment_sets:
+        digit_segment_sets[digit] = set()
+        for proper_segment in proper_digit_segment_sets[digit]:
+            digit_segment_sets[digit].add(segment_mapping[proper_segment])
 
-# def get_base_7_segment_dict():
-#     """Dictionary to hold the value for "correct" seven segment values."""
-#     signal_pattern_length_unique_digits = {
-#         2: 1,
-#         3: 7,
-#         4: 4,
-#         7: 8
-#     }
-#     base_dict = {
-#         'a': None,
-#         'b': None,
-#         'c': None,
-#         'd': None,
-#         'e': None,
-#         'f': None,
-#         'g': None,
-#     }
-#     return base_dict
+    return digit_segment_sets
