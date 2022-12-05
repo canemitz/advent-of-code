8b8b0869eee271af1717c1e6a43bda5749c49496
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-04 21:36:42 -0500

    Add 2022 day 4 part 2 solution.

--- 2022/day4.py
+++ 2022/day4.py
@@ -20,8 +20,20 @@ def part1(puzzle_input):
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print('Q: In how many assignment pairs do the ranges overlap?')
 
+    partial_overlap_count = 0
+    for i in range(0, len(puzzle_input)):
+        (pair_1, pair_2) = puzzle_input[i].split(',')
+        pair_1 = pair_1.split('-')
+        pair_2 = pair_2.split('-')
 
+        sections_1 = { x for x in range(int(pair_1[0]), int(pair_1[1]) +1) }
+        sections_2 = { x for x in range(int(pair_2[0]), int(pair_2[1]) +1) }
+
+        if bool(sections_1.intersection(sections_2)):
+            partial_overlap_count += 1
+
+    ans = partial_overlap_count
 
     print(f'A: {ans}')
