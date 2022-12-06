e1705e716ae9c2e255b28ee54af9f5bbe0d6a4be
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-05 23:23:06 -0500

    Add 2022 day 5 part 2 solution.

--- 2022/day5.py
+++ 2022/day5.py
@@ -33,7 +33,7 @@ def part1(puzzle_input):
 
     for procedure in rearrangement_procedure:
         for n in range(procedure[0]):
-            crates = move_crate(crates, procedure[1], procedure[2])
+            crates = move_crates(crates, procedure[1], procedure[2])
 
     ans = ''
     for i in range(1, 10):
@@ -43,19 +43,33 @@ def part1(puzzle_input):
     print(f'A: {ans}')
 
 
-def move_crate(crates, from_stack, to_stack):
-    try:
-        crate_moving = crates[from_stack].pop()
-        crates[to_stack].append(crate_moving)
-    except:
-        pass
+def move_crates(crates, from_stack, to_stack, how_many=1):
+    crates_to_move = []
+
+    for n in range(how_many):
+        try:
+            crate_n = crates[from_stack].pop()
+            crates_to_move.insert(0, crate_n)
+        except:
+            pass
+
+    crates[to_stack] += crates_to_move
 
     return crates
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print('Q: Using the CrateMover 9001, after the rearrangement procedure completes, what crate ends up on top of each stack?')
 
+    crates = puzzle_input['crates']
+    rearrangement_procedure = puzzle_input['rearrangement_procedure']
 
+    for procedure in rearrangement_procedure:
+        crates = move_crates(crates, procedure[1], procedure[2], procedure[0])
+
+    ans = ''
+    for i in range(1, 10):
+        if len(crates[i]):
+            ans += crates[i].pop()
 
     print(f'A: {ans}')
