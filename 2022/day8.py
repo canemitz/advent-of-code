d11d4e86e03809b5fad18b9af6cbf4282957b225
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-08 20:34:44 -0500

    Add 2022 day 8 part 2 solution, and custom example input.

--- 2022/day8.py
+++ 2022/day8.py
@@ -52,8 +52,81 @@ def tree_is_visible(tree_grid, i, j):
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print('Q: Consider each tree on your map. What is the highest scenic score possible for any tree?')
 
+    tree_grid = puzzle_input
 
+    max_y = (len(tree_grid)    - 1) / 2
+    max_x = (len(tree_grid[0]) - 1) / 2
+    scenic_score_theoretical_max = int(max_y**2 * max_x**2)
+
+    ans = 1
+    for i in range(len(tree_grid)):
+        row = tree_grid[i]
+        for j in range(len(row)):
+            ans = max(ans, get_scenic_score(tree_grid, i, j))
+
+        if ans == scenic_score_theoretical_max:
+            break
 
     print(f'A: {ans}')
+
+
+def get_scenic_score(tree_grid, i, j):
+    """Return scenic score for tree at (i,j), which is the produce of the viewing distances in each direction.
+
+    Note: the viewing distance is the distance at which a tree equals or exceeds the tree at (i,j);
+          this means that there could be trees in some direction that block ones farther out, but don't block your view,
+          so you might not be able to see all the trees in your viewing distance.""
+    """
+    tree_height = tree_grid[i][j]
+
+    viewing_distance = {
+        'left' : 0,
+        'right': 0,
+        'up'   : 0,
+        'down' : 0
+    }
+
+    trees_left  = tree_grid[i][:j]
+    trees_right = tree_grid[i][j+1:]
+    if not trees_left or not trees_right:
+        return 0
+
+    for tree in trees_left[::-1]:
+        viewing_distance['left'] += 1
+        if tree >= tree_height:
+            break
+
+    for tree in trees_right:
+        viewing_distance['right'] += 1
+        if tree >= tree_height:
+            break
+
+    trees_up   = []
+    trees_down = []
+    for row_idx in range(len(tree_grid)):
+        row = tree_grid[row_idx]
+        if row_idx < i:
+            trees_up.append(row[j])
+        elif row_idx > i:
+            trees_down.append(row[j])
+
+    if not trees_up or not trees_down:
+        return 0
+
+    for tree in trees_up[::-1]:
+        viewing_distance['up'] += 1
+        if tree >= tree_height:
+            break
+
+    for tree in trees_down:
+        viewing_distance['down'] += 1
+        if tree >= tree_height:
+            break
+
+    scenic_score = 1
+    for direction in viewing_distance:
+        scenic_score *= viewing_distance[direction]
+
+    return scenic_score
