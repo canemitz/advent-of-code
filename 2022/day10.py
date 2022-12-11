f2c7b2e652c4a3227502f063d8fb10e1d04ee474
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-11 13:02:04 -0500

    Add 2022 day 10 part 2 solution.

--- 2022/day10.py
+++ 2022/day10.py
@@ -40,8 +40,123 @@ def part1(puzzle_input):
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print('Q: Render the image given by your program. What eight capital letters appear on your CRT?')
+
+    crt_width  = 40
+    crt_height = 6
+    num_pixels = crt_width * crt_height
+
+    # Position of center sprite pixel
+    X = 1
+    sprite_width = 3
+    sprite_offset = (sprite_width - 1) / 2
+    busy = 0
+    pending_add = 0
+    row_i = 0
+
+    ans = ''
+    for pixel_i in range(num_pixels):
+        # Move to next row after end of line
+        try:
+            if cycle % crt_width == 0:
+                row_i += 1
+                ans += '\n'
+        except:
+            pass
+
+        cycle = pixel_i + 1
+
+        if not busy:
+            # Get next instruction
+            line = puzzle_input.pop(0)
+            try:
+                # addx
+                command, pending_add = line.split()
+                busy = 2
+            except:
+                # noop
+                command = line
+
+        pixel = ' '
+        if (X - sprite_offset) <= (pixel_i - (crt_width*row_i)) <= (X + sprite_offset):
+            pixel = '█'
+        ans += pixel
+
+        if busy:
+            busy -= 1
+
+        if not busy and pending_add:
+            X += int(pending_add)
+            pending_add = 0
+
+    print(f'A: \n{ans}')
 
 
+def part2_debug(puzzle_input):
+    """Copy of part2, but pauses after each cycle, and prints out information as in Eric's example for easy comparison."""
+    print('Q: Render the image given by your program. What eight capital letters appear on your CRT?')
 
-    print(f'A: {ans}')
+    crt_width  = 40
+    crt_height = 6
+    num_pixels = crt_width * crt_height
+
+    # Position of center sprite pixel
+    X = 1
+    sprite_width = 3
+    sprite_offset = (sprite_width - 1) / 2
+    busy = 0
+    pending_add = 0
+    row_i = 0
+
+    ans = ''
+    for pixel_i in range(num_pixels):
+        # Move to next row after end of line
+        try:
+            if cycle % crt_width == 0:
+                row_i += 1
+                ans += '\n'
+        except:
+            pass
+
+        cycle = pixel_i + 1
+
+        if not busy:
+            # Get next instruction
+            line = puzzle_input.pop(0)
+            try:
+                # addx
+                command, pending_add = line.split()
+                busy = 2
+                print(f'Start cycle {str(cycle).rjust(3)}: begin executing {command} {pending_add}')
+            except:
+                # noop
+                command = line
+                print(f'Start cycle {str(cycle).rjust(3)}: begin executing {command}')
+
+        pixel = '.'
+
+        if (X - sprite_offset) <= (pixel_i - (crt_width*row_i)) <= (X + sprite_offset):
+            pixel = '#'
+        ans += pixel
+        print(f'During cycle {str(cycle).rjust(2)}: CRT draws pixel in position {pixel_i}')
+
+        current_crt_row = ans.split('\n').pop()
+        print(f'Current CRT row: {current_crt_row}')
+
+        if busy:
+            busy -= 1
+
+        if not busy and pending_add:
+            X += int(pending_add)
+            print(f'End of cycle {str(cycle).rjust(1)}: finish executing addx {pending_add} (Register X is now {X})')
+            pending_add = 0
+            sprite_position = ['.' for x in range(X-1) ]
+            sprite_position += ['###']
+            sprite_position += ['.' for x in range (X+1, crt_width)]
+            print(f'Sprite position: {"".join(sprite_position)}')
+        elif not busy:
+            print(f'End of cycle {str(cycle).rjust(1)}: finish executing noop')
+
+        input()
+
+    print(f'A: \n{ans}')
