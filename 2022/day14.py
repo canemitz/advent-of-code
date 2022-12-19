82ab8581bc41a4bf5e036dcb4d75dd397d481ce0
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-19 15:30:54 -0500

    Add 2022 day 14 part 2 solution. Don't create initial map taller than is necessary.

--- 2022/day14.py
+++ 2022/day14.py
@@ -2,12 +2,17 @@ import numpy as np
 
 
 def part1(puzzle_input):
-    print('Q: Using your scan, simulate the falling sand. How many units of sand come to rest before sand starts flowing into the abyss below?')
+    if not running_part2():
+        print('Q: Using your scan, simulate the falling sand. How many units of sand come to rest before sand starts flowing into the abyss below?')
 
     rock_map = get_initial_rock_map(puzzle_input)
     sand_src_y = 0
     sand_src_x = 500
     rock_map[sand_src_y, sand_src_x] = '+'
+
+    # Add floor to bottom of rock map for part 2
+    if running_part2():
+        rock_map = add_floor_to_rock_map(rock_map)
 
     # Drop sand grains until the map doesn't change
     num_sand_grains = 0
@@ -34,7 +39,7 @@ def get_initial_rock_map(puzzle_input):
         for point in rock_path.split(' -> '):
             [x, y] = [ int(coord) for coord in point.split(',') ]
             max_x = max(x+1, max_x)
-            max_y = max(y+1, max_x)
+            max_y = max(y+1, max_y)
 
     # Create rock_map array big enough to hold all coords, filled with air
     rock_map = np.full([max_y, max_x], '.', dtype=str)
@@ -106,8 +111,39 @@ def drop_sand_grain(rock_map, sand_y, sand_x):
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print('Q: Using your scan, simulate the falling sand until the source of the sand becomes blocked. How many units of sand come to rest?')
+
+    global part
+    part = 2
+
+    part1(puzzle_input)
 
 
+def add_floor_to_rock_map(rock_map):
+    """Add an [effectively] infinitely wide floor of rock at bottom of rock map."""
+    y_max, x_max = rock_map.shape
+    sand_src_x = np.where(rock_map == '+')[1][0]
 
-    print(f'A: {ans}')
+    # The farthest out from the sand source that a grain could fall from the source is equal to the height of the map on either side
+    extra_columns_needed = sand_src_x + y_max - x_max + 2
+
+    # Add columns of air to the right of the map (the src is already far enough from the left of the map)
+    if extra_columns_needed > 0:
+        rock_map = np.pad(rock_map, ((0, 0), (0, extra_columns_needed)), 'constant', constant_values='.')
+
+    # Add a row of air, and a row of rock
+    air_row  = [ '.' for x in range(rock_map.shape[1]) ]
+    rock_row = [ '#' for x in range(rock_map.shape[1]) ]
+    rock_map = np.row_stack([rock_map, air_row, rock_row])
+
+    return rock_map
+
+
+def running_part2():
+    """We define the global part in part2, but not part1, so we can test whether that variable is defined to determine which part we're running."""
+    global part
+
+    try:
+        return part == 2
+    except:
+        return False
