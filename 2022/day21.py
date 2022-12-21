4968b73e5d6b5dedb457fbf03f87bf78a91fa827
Clayton Nemitz <clay.nemitz@gmail.com>
2022-12-21 14:41:17 -0500

    Add 2022 day 21 part 2 solution. Rework monkey_yells to allow it to return an expression that includes other monkey names instead of just an integer.

--- 2022/day21.py
+++ 2022/day21.py
@@ -1,3 +1,7 @@
+import re
+from sympy import simplify, solve
+
+
 def part1(puzzle_input):
     print('Q: What number will the monkey named root yell?')
 
@@ -10,15 +14,29 @@ def part1(puzzle_input):
 
 
 def monkey_yells(name):
+    """Update global monkeys dictionary with integer value or symbolic expression of what the monkey yells."""
     global monkeys
 
-    job = monkeys[name]
+    # If a monkey doesn't have a job, just return the monkey's name (only used for 'humn')
+    try:
+        job = monkeys[name]
+    except:
+        return name
+
     if type(job) is int:
         this_monkey_yells = job
     else:
-        monkey_a, operator, monkey_b = job.split()
-        this_monkey_yells = eval(f'{monkey_yells(monkey_a)} {operator} {monkey_yells(monkey_b)}')
-        this_monkey_yells = int(this_monkey_yells)
+        # Replace monkey names in this job with their current value or expression
+        monkey_names = re.findall(r'[a-z]{4}', job)
+        for name in monkey_names:
+            job = job.replace(name, str(monkey_yells(name)))
+
+        # Attempt to reduce the length of this expression
+        try:
+            this_monkey_yells = eval(job)
+            this_monkey_yells = int(this_monkey_yells)
+        except:
+            this_monkey_yells = f'({simplify(job)})'
 
     monkeys[name] = this_monkey_yells
 
@@ -26,9 +44,17 @@ def monkey_yells(name):
 
 
 def part2(puzzle_input):
-    print('Q:')
+    print("Q: What number do you yell to pass root's equality test?")
 
+    global monkeys
+    monkeys = puzzle_input
+    del monkeys['humn']
 
+    root_a, _, root_b = monkeys['root'].split()
+    root_a_expr = monkey_yells(root_a)
+    root_b_expr = monkey_yells(root_b)
+
+    ans = solve( f'{root_a_expr} - {root_b_expr}' )[0]
 
     print(f'A: {ans}')
 
